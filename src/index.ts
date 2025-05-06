80560a3a00a614d82e0dfadad40ed3aaf6d1797e
80LK <80lkru@gmail.com>
2025-05-06 07:40:12 +0300

    run process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import { spawn } from "child_process";
+import { dirname, resolve } from "path";
+
+function main() {
+	if (process.argv.length == 2) {
+		console.log(`Usage: slc_wrapper.exe <executable> [--wd=<workdir>] [options]`);
+		return;
+	}
+
+	const CWD = process.cwd();
+	const executable = resolve(CWD, process.argv[2]);
+	const cwd = resolve(CWD, process.argv.find(value => value.startsWith('--wd='))?.slice(5) ?? dirname(executable));
+	const args = process.argv.slice(3).filter(v => !v.startsWith('--wd='));
+
+	spawn(executable, args, { cwd })
+}
+
+
+main();
